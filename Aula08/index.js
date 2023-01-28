0ddee03b0337a0642768bcd4dbbe473f8e8ac3ad
Natanael Vieira <natanaelsales@live.com>
2023-01-28 05:00:34 -0300

    Update index.js

--- Aula08/index.js
+++ Aula08/index.js
@@ -1,9 +1,29 @@
 'use strict'
 
+
+class Produto {
+    codigo;
+    nome;
+    constructor (codigo, nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+}
+
 class Disciplina {
     codigo;
     nome;
     constructor(codigo, nome) {
-
+        this.codigo = codigo;
+        this.nome = nome;
     }
 }
+
+const d1 = new Disciplina(123, 'Matemática');
+console.log(d1.codigo)
+
+const p1 = new Produto(456, 'Prod XPTO')
+console.log(p1.nome)
+
+const d2 = new Disciplina(789, 'Lógica POO')
+console.log(d2.codigo)
