62daafaf88da028f3e4aafa68169ebe173cf44d6
Natanael Vieira <natanaelsales@live.com>
2023-01-30 05:18:07 -0300

    Update index.js

--- Aula10/index.js
+++ Aula10/index.js
@@ -17,42 +17,22 @@ console.log(Musica.formato);
 
 //###################################################
 class Produto {
-    static vermelho = 'RED'
+    static GRANDE = 10;
+    static MEDIO = 5;
+    static MEDIO = 1;
     codigo;
     titulo;
     valor;
-    constructor (codigo, titulo, valor) {
+    constructor (codigo, titulo, valor, tamanho) {
         this.codigo = codigo;
         this.titulo = titulo;
         this.valor = valor;
+        this.tamanho = tamanho;
     }
 }
 
-class Disciplina {
-    codigo;
-    nome;
-    nota;
-    constructor(codigo, nome, nota) {
-        this.codigo = codigo;
-        this.nome = nome;
-        this.nota = nota;
-    }
-}
-
-const d1 = new Disciplina(123, 'Matemática', 8);
-console.log(d1);
-
-const d2 = d1;
-console.log(d2);
-
-d2.codigo = 444;
-d2.nome = 'Álgebra';
-d2.nota = 9;
-
-console.log(d1)
-
-const p1 = new Produto(337, "Meu Produto", 54);
-console.log(p1.vermelho); /* Neste caso eu deveria utilizar a classe e não o 
+const p1 = new Produto(337, "Meu Produto", 54, Produto.GRANDE);
+console.log(p1); /* Neste caso eu deveria utilizar a classe e não o 
 produto que foi criado para acessar as informações ocultas. Podemos considerar
 ela como uma constante, que não será mudada, pois neste caso de classes não tenho
 como criar uma variável let ou const dentro do escopo da classe.*/ 
