0e4733b0fe01e96c1e55b1d44d93d11eef93353d
Natanael Vieira <natanaelsales@live.com>
2023-02-09 19:03:58 -0300

    Update index.js

--- Aula16/index.js
+++ Aula16/index.js
@@ -25,3 +25,11 @@ const lab = new Laboratorio('Informática', 37);
 console.log(lab.getCapacidade())
 lab.nome = 'Computação;'
 console.log(lab.nome) // Só vai aparecer no console porque foi atribuído um valor logo acima, mas que fora isso, como o atributo está privado ele não foi alterado. Neste caso para alterar o atributo nome eu precisaria incluir a palavra reservada set
+console.log(lab);
+
+const lab2 = new Laboratorio ('Meu Lab', 71);
+console.log(lab2);
+lab2.setNome('Mudando Nome');
+console.log(lab2);
+lab2.setCapacidade(91);
+console.log(lab2)
