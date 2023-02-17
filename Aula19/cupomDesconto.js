0c1cef3e3b93772e6e667c6fd535e13d4a3f9de5
Natanael Vieira <natanaelsales@live.com>
2023-02-17 06:48:46 -0300

    Update cupomDesconto.js

--- Aula19/cupomDesconto.js
+++ Aula19/cupomDesconto.js
@@ -3,9 +3,9 @@ export class CupomDesconto {
     #desconto2 = 20;
     constructor() { }
     getDesconto(gastos) {
-        if(gastos >= 100 && gastos <= 3000) {
+        if(gastos >= 5000 && gastos <= 10000) {
             console.log(`Você tem direito a ${this.#desconto1}% de desconto`);
-        } else if (gastos > 3000) {
+        } else if (gastos > 10000) {
             console.log(`Você tem direito a ${this.#desconto2}% de desconto`)
         } else {
             console.log('Infelizmente você não possui descontos no momento.')
