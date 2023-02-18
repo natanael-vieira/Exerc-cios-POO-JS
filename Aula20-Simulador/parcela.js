c48f0c8c12b4ce9b885626f4d73a09f208c3fbd3
Natanael Vieira <natanaelsales@live.com>
2023-02-18 07:28:44 -0300

    Update parcela.js

--- Aula20-Simulador/parcela.js
+++ Aula20-Simulador/parcela.js
@@ -14,4 +14,13 @@ export class Parcela {
     getSaldo() {
         return this.#saldo;
     }
+    getDadosFormatados() {
+        const dados = [];
+        dados.push(this.#numero); 
+        dados.push(this.#valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        dados.push(this.#amortizacao.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        dados.push(this.#juros.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        dados.push(this.#saldo.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        return dados
+    }
 }
