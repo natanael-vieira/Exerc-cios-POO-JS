675f7849674d5eb161369d8e7f25b0f87721b44a
Natanael Vieira <natanaelsales@live.com>
2023-02-18 06:11:24 -0300

    Update financiamento.js

--- Aula20-Simulador/financiamento.js
+++ Aula20-Simulador/financiamento.js
@@ -4,4 +4,13 @@ export class Financiamento {
     #taxaJuros; //juros mensais
     #prazo; //em meses
     #parcelas = [];
+    constructor(valor, entrada, taxaJuros, prazo) {
+        this.#taxaJuros = taxaJuros;
+        this.#prazo = prazo;
+        this.#parcelas.push(new Parcela(0, 0, 0, 0, valor - entrada))
+    }
+    calcParcelasMensais() {
+        let saldo = this.#parcelas[this.#parcelas.length - 1].getSaldo();
+        let prazo = this.#prazo - (this.#parcelas.length - 1);
+    }
 }
